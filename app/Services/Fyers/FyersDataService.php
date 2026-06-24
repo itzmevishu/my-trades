4fca5fb57a4c1ae9b5c4828cedeb38893cc3e046
Vishal Kalappagari <vkalappagari@medallia.com>
2026-06-24 10:57:40 +0530

    Fix: Use api-t1.fyers.in/data endpoint for User App

--- app/Services/Fyers/FyersDataService.php
+++ app/Services/Fyers/FyersDataService.php
@@ -17,7 +17,7 @@ use Carbon\Carbon;
  */
 class FyersDataService extends BaseService
 {
-    private string $baseUrl = 'https://api-t2.fyers.in/data-rest/v3';
+    private string $baseUrl = 'https://api-t1.fyers.in/data';
     private FyersAuthService $authService;
     private RateLimiter $rateLimiter;
     
