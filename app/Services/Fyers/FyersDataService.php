5e8e6ad42a7c715192a1518bb0b107a03e6300ef
Vishal Kalappagari <vkalappagari@medallia.com>
2026-06-24 10:52:52 +0530

    Fix: Use correct Fyers data API endpoint (api-t2.fyers.in/data-rest/v2)

--- app/Services/Fyers/FyersDataService.php
+++ app/Services/Fyers/FyersDataService.php
@@ -17,7 +17,7 @@ use Carbon\Carbon;
  */
 class FyersDataService extends BaseService
 {
-    private string $baseUrl = 'https://api.fyers.in/data-rest/v3';
+    private string $baseUrl = 'https://api-t2.fyers.in/data-rest/v2';
     private FyersAuthService $authService;
     private RateLimiter $rateLimiter;
     
